6d795a329152d85d6b72990909e78fbfc673994d
Julio C. Rocha <dev@rocha.red>
2023-10-05 00:20:23 -0700

    Init

---
+++ WifiSwitch/NativeApis.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Hyvart.WifiManager
+{
+    // http://jonathanpeppers.com/Blog/xamarin-ios-under-the-hood-calling-objective-c-from-csharp
+    public static class NativeApis
+	{
+		[DllImport("/usr/lib/libobjc.dylib", EntryPoint = "objc_getClass")]
+		public static extern IntPtr IntPtr_objc_getClass(IntPtr receiver, IntPtr selector);
+    }
+}
+
