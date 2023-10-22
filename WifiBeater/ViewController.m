451b7ce2a46e136e3c818e6c4df1ae410625875d
Julio C. Rocha <dev@rocha.red>
2023-10-21 20:49:38 -0700

    Add iphoneheaders to  search paths

--- WifiBeater/ViewController.m
+++ WifiBeater/ViewController.m
@@ -7,6 +7,9 @@
 
 #import "ViewController.h"
 
+// Private iOS headers
+#import <SpringBoard/SBWiFiManager.h>
+
 #import <objc/runtime.h>
 
 @interface ViewController ()
@@ -22,9 +25,7 @@
 
 //https://stackoverflow.com/questions/2053114
 - (IBAction)wifiSwitchToggled:(id)sender {
-    
-    // https://github.com/nanotech/iphoneheaders/blob/master/SpringBoard/SBWiFiManager.h
-    //[[objc_getClass("SBWiFiManager") sharedInstance] setWiFiEnabled:NO];
+    [[objc_getClass("SBWiFiManager") sharedInstance] setWiFiEnabled:NO];
 }
 
 @end
